22cdabacae806b645ac68eb09e8562d828b28194
Byron Thomas <byron.thomas@gmail.com>
2025-09-01 15:29:29 +0100

    Update test code to a more interesting version

--- test-project/main.go
+++ test-project/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	struct1 := &Struct1{field: 1}
+	struct2 := &Struct2{fieldX: 2, fieldY: 3}
+	result, err := check(struct1)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Result:", result)
+	}
+	result, err = check(struct2)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("Result:", result)
+}
+
+type Getter interface {
+	Get() int
+}
+
+type Struct1 struct {
+	field int
+}
+
+func (s *Struct1) Get() int {
+	return s.field
+}
+
+type Struct2 struct {
+	fieldX int
+	fieldY int
+}
+
+func (s *Struct2) Get() int {
+	return s.fieldX + s.fieldY
+}
+
+func check(getter Getter) (int, error) {
+	res := getter.Get()
+	if res < 0 {
+		return 0, errors.New("negative result")
+	}
+	return res, nil
 }
